07f55219da80eea6029e55d1e43e90e0fd90a571
pavsoldatov <snowschoe@gmail.com>
2022-07-15 11:11:25 +0300

    Add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+1. Clone the repo
+2. `npm install` to install dependencies
+3. `npm start` to start the server
+4. ???
+5. PTOFIT
--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -1,19 +1,13 @@
 import styles from "./UserItem.module.css";
 
 const UserItem = (props) => {
-  const id = Math.random().toString();
-
-  const clickHandler = (e) => {
+  const getId = (e) => {
     const id = e.target.closest("span").id;
-    props.onGetUserID(id)
+    props.onGetUserID(id);
   };
 
   return (
-    <span
-      className={styles.userItem}
-      id={props.id}
-      onClick={clickHandler}
-    >
+    <span className={styles.userItem} id={props.id} onClick={getId}>
       <div className={styles.userName}>{props.name}</div>
       <div className={styles.userAge}>{`${props.age} ${
         props.age === "1" ? "year" : "years"
